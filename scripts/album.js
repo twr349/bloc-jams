c736c4afcacd3187119f2e53ae4faed1b4904d61
twr349 <twr349@gmail.com>
2017-08-02 08:54:59 -0700

    Button glitch

--- scripts/album.js
+++ scripts/album.js
@@ -49,14 +49,14 @@ var albumDjango = {
  var createSongRow = function(songNumber, songName, songLength) {
     var template =
         '<tr class="album-view-song-item">'
-       +'   <td class="song-item-number" data-song-number="' + songNumber +'">' + songNumber + '</td>'
+       +'  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
        +'   <td class="song-item-title">' + songName + '</td>'
        +'   <td class="song-item-duration">' + songLength + '</td>'
        +'</tr>'
        ;
 
        return template;
- }
+ };
 
  var albumTitle = document.getElementsByClassName('album-view-title')[0];
  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
@@ -79,24 +79,30 @@ var albumDjango = {
     }
  };
 
+
+
+
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
+
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
 
 songListContainer.addEventListener('mouseover', function(event) {
     if (event.target.parentElement.className === 'album-view-song-item') {
-      event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
     }
 });
 
 for (var i = 0; i < songRows.length; i++) {
-    songRows[i].addEventListener('mouseleave', function(event) {
-      this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
-    });
+         songRows[i].addEventListener('mouseleave', function(event) {
+        this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
 }
 
     var albums = [albumPicasso, albumMarconi, albumDjango];
@@ -108,4 +114,3 @@ for (var i = 0; i < songRows.length; i++) {
               index = 0;
             }
       });
-};
