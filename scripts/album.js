a60a23e4b791ae5f5303e0eab82abd9455c5c24f
twr349 <twr349@gmail.com>
2017-08-04 15:33:59 -0700

    Checkpoint 17 jQuery Collection and Album Views Completed

--- scripts/album.js
+++ scripts/album.js
@@ -55,27 +55,28 @@ var albumDjango = {
        +'</tr>'
        ;
 
-       return template;
+       return $(template);
  };
 
- var albumTitle = document.getElementsByClassName('album-view-title')[0];
- var albumArtist = document.getElementsByClassName('album-view-artist')[0];
- var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
- var albumImage = document.getElementsByClassName('album-cover-art')[0];
- var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ var $albumTitle = $('.album-view-title');
+ var $albumArtist = $('.album-view-artist');
+ var $albumReleaseInfo = $('.album-view-release-info');
+ var $albumImage = $('.album-cover-art');
+ var $albumSongList = $('.album-view-song-list');
 
 
  var setCurrentAlbum = function(album) {
 
-    albumTitle.firstChild.nodeValue = album.title;
-    albumArtist.firstChild.nodeValue = album.artist;
-    albumReleaseInfo.firstChild.nodeValue= album.year + ' ' + album.label;
-    albumImage.setAttribute('src', album.albumArtUrl);
+    $albumTitle.text(album.title);
+    $albumArtist.text(album.artist);
+    $albumReleaseInfo.text(album.year + ' ' + album.label);
+    $albumImage.attr('src', album.albumArtUrl);
 
-    albumSongList.innerHTML = '';
+    $albumSongList.empty();
 
     for (var i = 0; i < album.songs.length; i++) {
-      albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+      var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+      $albumSongList.append($newRow);
     }
  };
 
@@ -172,7 +173,7 @@ for (var i = 0; i < songRows.length; i++) {
         clickHandler(event.target);
      });
 }
-
+var albumImage = document.getElementsByClassName('album-cover-art')[0]; //Temp variable
     var albums = [albumPicasso, albumMarconi, albumDjango];
     var index = 1;
     albumImage.addEventListener('click', function(event) {
