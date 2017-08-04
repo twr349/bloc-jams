b339a4222306851045773e19b4e48fc7be363d0b
twr349 <twr349@gmail.com>
2017-08-04 08:32:14 -0700

    Assignment 13 fix 2

--- scripts/album.js
+++ scripts/album.js
@@ -133,7 +133,7 @@ var clickHandler = function(targetElement) {
  };
 
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-var songRows = document.getElementsByClassName('album-view-song-item');
+var songRows = document.getElementsByClassName('song-item-number');
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
